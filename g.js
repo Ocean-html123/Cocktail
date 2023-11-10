0a0b16b63beb0959756a59671851a4335c10a560
ocean <okparachukwuemmanuel@gmail.com>
2023-11-10 10:45:09 -0800

    main commit

---
+++ g.js
@@ -0,0 +1,53 @@
+const icon = document.querySelector('.nav')
+const side = document.querySelector('.side-i')
+const sidebar = document.querySelector('.side-bar')
+const bar = document.querySelector('.side')
+const loader = document.querySelector('.loader-1')
+const grid = document.querySelector('.grid')
+icon.addEventListener('click', () => {
+    if (!sidebar.classList.contains('show-sidebar')) {
+        sidebar.classList.add('show-sidebar')
+    }
+})
+
+side.addEventListener('click', () => {
+    if (sidebar.classList.contains('show-sidebar')) {
+        sidebar.classList.remove('show-sidebar')
+    }
+})
+
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g'
+ const fetchData = async () => {
+    try {
+         loader.innerHTML = `<div class="loader"></div>`
+         const response = await fetch(url)
+         const cocktail = await response.json()
+         const product = cocktail.drinks
+       loader.innerHTML = ''
+       console.log(product);
+       const ocean = product.map((items) => {
+  const {idDrink: id, strDrink: Name, strDrinkThumb: images, 
+    strIngredient1: ingredient1,  strIngredient2: ingredient2,  strIngredient3: ingredient3} = items
+    return` <a href="">
+    <div class="image">
+        <img src="${images}" alt="" class="img">
+   <h4>${Name}</h4>
+   <h5>Ingredients</h5>
+   <ul>
+  <li>${ingredient1}</li>
+  <li>${ingredient2}</li>
+  <li>${ingredient3}</li>
+   </ul>
+   <div class="btn">
+   <button>Add to Cart</button>
+   </div>
+    </div>
+  </a>`
+       }).join('')
+       grid.innerHTML = `${ocean}`
+    } catch (error) {
+        
+    }
+ }
+ fetchData();
